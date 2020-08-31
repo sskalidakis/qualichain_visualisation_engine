105423ddfaebf609b4fdc328263a5a842847f7f6
epu_temp_steve <sskalidakis@epu.ntua.gr>
2020-08-31 18:38:52 +0300

    popular skills bar_chart popular courses bar_chart

--- data_manager/manager.py
+++ data_manager/manager.py
@@ -120,16 +120,78 @@ def group_users_per_column(column, aggregation="count"):
 
 def build_bar_chart(base_query, x_axis_name, **kwargs):
     """This abstract function is used to call submethods/specific model"""
+    bar_chart_input = []
     if base_query == 'group_users':
         bar_chart_input = group_users_per_column(x_axis_name)
+    elif base_query == 'popular_skills':
+        bar_chart_input = popular_skills()
+    elif base_query == 'popular_courses':
+        bar_chart_input = popular_courses()
     return bar_chart_input
 
-# def courses_most_popular_skills(column, aggregation="count", number_of_courses=15):
-#     """This function is used to find courses with the most popular skills"""
-#
-#     job_skills_df = pd.read_sql_table('job_skills', ENGINE_STRING)
-#     group = job_skills_df[[column, 'id']].groupby('skill_id').agg(aggregation).reset_index()
-#     import pdb
-#     pdb.set_trace()
-#     final_values = list(group.to_dict('index').values())
-#     return final_values
+
+
+
+
+def popular_skills(most_popular_skills=10):
+    """This function is used to find the most popular skills"""
+
+    job_skills_df = pd.read_sql_table('job_skills', ENGINE_STRING)
+    grouped_job_skills_df = job_skills_df[['skill_id', 'id']].groupby('skill_id').size().reset_index(
+        name='count').sort_values('count')
+    skills_df = pd.read_sql_table('skills', ENGINE_STRING).rename(columns={'id': 'skill_id', 'name': 'skill_name'})
+    popular_skills_df = pd.merge(grouped_job_skills_df, skills_df, how='left', on='skill_id')[['skill_name', 'count']]
+    final_values = list(popular_skills_df.to_dict('index').values())
+    print(final_values)
+    return final_values
+
+
+def popular_courses(number_of_courses=10):
+    """This function is used to find the most popular courses according to job market"""
+
+    job_skills_df = pd.read_sql_table('job_skills', ENGINE_STRING)
+    grouped_job_skills_df = job_skills_df[['skill_id', 'id']].groupby('skill_id').size().reset_index(
+        name='count').sort_values('count').tail()
+    skills_df = pd.read_sql_table('skills', ENGINE_STRING).rename(columns={'id': 'skill_id'})
+    popular_skills_df = pd.merge(grouped_job_skills_df, skills_df, how='left', on='skill_id')[
+        ['skill_id', 'count']]
+    skills_courses_df = pd.read_sql_table('skills_courses', ENGINE_STRING)
+    skills_courses_count = pd.merge(popular_skills_df, skills_courses_df, how='left', on='skill_id')[
+        ['course_id', 'count']].groupby(['course_id'])['count'].agg('max')
+    courses_df = pd.read_sql_table('courses', ENGINE_STRING).rename(columns={'id': 'course_id', 'name': 'course_name'})
+    popular_courses = pd.merge(skills_courses_count, courses_df, how='left', on='course_id')[
+        ['course_name', 'count']].sort_values('course_name').tail(number_of_courses)
+    final_values = list(popular_courses.to_dict('index').values())
+    print(final_values)
+    return final_values
+
+
+def api_most_popular_courses(number_of_courses=100, most_popular_skills=10):
+    """This api is used to find courses with the most popular skills"""
+
+    job_skills_df = pd.read_sql_table('job_skills', ENGINE_STRING)
+    grouped_job_skills_df = job_skills_df[['skill_id', 'id']].groupby('skill_id').size().reset_index(
+        name='count').sort_values('count').tail(most_popular_skills)
+    skills_df = pd.read_sql_table('skills', ENGINE_STRING).rename(columns={'id': 'skill_id', 'name': 'skill_name'})
+    popular_skills_df = pd.merge(grouped_job_skills_df, skills_df, how='left', on='skill_id')[
+        ['skill_id', 'skill_name', 'count']]
+    skills_courses_df = pd.read_sql_table('skills_courses', ENGINE_STRING)
+    skills_courses_count = pd.merge(popular_skills_df, skills_courses_df, how='left', on='skill_id')[
+        ['course_id', 'skill_name', 'count']].sort_values('count').tail(number_of_courses)
+    courses_df = pd.read_sql_table('courses', ENGINE_STRING).rename(columns={'id': 'course_id', 'name': 'course_name'})
+    popular_courses = pd.merge(skills_courses_count, courses_df, how='left', on='course_id')[
+        ['course_name', 'skill_name', 'count']].sort_values('course_name')
+    final_values = []
+    course_name = ''
+    new_dict = {}
+    for index, row in popular_courses.iterrows():
+        if course_name == '' or course_name != row['course_name']:
+            if course_name != '':
+                final_values.append(new_dict)
+            course_name = row['course_name']
+            new_dict['course'] = course_name
+            new_dict[row['skill_name']] = row['count']
+        else:
+            new_dict[row['skill_name']] = row['count']
+
+    return final_values
