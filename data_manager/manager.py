315b9b8609c8c218de8e45cb4089b3aa51b00fc9
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-08-31 17:11:49 +0300

    some changes on manager

--- data_manager/manager.py
+++ data_manager/manager.py
@@ -110,18 +110,55 @@ def heatmap_ordering(order, variables, var_position):
     return ordering
 
 
+def get_table(**kwargs):
+    """
+    This function is used to load the provided table as a Pandas DataFrame
+
+    :param kwargs: provided kwargs
+    :return: pandas DataFrame
+    """
+    if 'sql_command' in kwargs.keys():
+        sql_command = kwargs['sql_command']
+        table_df = pd.read_sql_query(sql_command, ENGINE_STRING)
+    elif 'table' in kwargs.keys():
+        table = kwargs['table']
+        table_df = pd.read_sql_table(table, ENGINE_STRING)
+    else:
+        table_df = pd.DataFrame()
+    return table_df
+
+
 def group_users_per_column(column, aggregation="count"):
     """This function is used to group users table according to provided column"""
-    users_df = pd.read_sql_table('users', ENGINE_STRING)
+
+    users_df = get_table(table='users')
     group = users_df[[column, 'id']].groupby(column).agg(aggregation).reset_index()
     final_values = list(group.to_dict('index').values())
     return final_values
+
+
+def user_jobs_groups(column, aggregation="count"):
+    """This function is used to aggregate jobs data for a specific user"""
+    sql_command = """SELECT job_id from user_applications where user_id={}""".format(1)
+    user_applied_jobs = get_table(sql_command=sql_command)
+    job_ids = user_applied_jobs["job_id"].to_list()
+    if len(job_ids) == 1:
+        fetch_jobs = """SELECT * from jobs where id in ({})""".format(job_ids[0])
+    else:
+        job_tuples = tuple(job_ids)
+        fetch_jobs = """SELECT * from jobs where id in {}""".format(job_tuples)
+    related_jobs = get_table(sql_command=fetch_jobs)
+    group = related_jobs[[column, "id"]].groupby(column).agg(aggregation).reset_index()
+    values = list(group.to_dict('index').values())
+    return values
 
 
 def build_bar_chart(base_query, x_axis_name, **kwargs):
     """This abstract function is used to call submethods/specific model"""
     if base_query == 'group_users':
         bar_chart_input = group_users_per_column(x_axis_name)
+    elif base_query == 'group_job_user':
+        bar_chart_input = user_jobs_groups(x_axis_name)
     return bar_chart_input
 
 # def courses_most_popular_skills(column, aggregation="count", number_of_courses=15):
