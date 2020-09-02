e481c84e20fda4b020eb71b69c9f33530f8b76c3
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-09-02 15:46:36 +0300

    Some changes

--- visualiser/views.py
+++ visualiser/views.py
@@ -6,8 +6,7 @@ from django.apps import apps
 
 from django.http import HttpResponse
 
-from data_manager.manager import create_heatmap_data, group_users_per_column, build_bar_chart, user_jobs_groups, \
-    salary_information
+from data_manager.manager import create_heatmap_data, group_users_per_column, build_bar_chart, user_jobs_groups
 from visualiser.fake_data.fake_data import FAKE_DATA, COLUMNCHART_DATA, BAR_RANGE_CHART_DATA, BAR_HEATMAP_DATA, \
     HEAT_MAP_DATA, SANKEYCHORD_DATA, THERMOMETER, HEAT_MAP_CHART_DATA, PARALLEL_COORDINATES_DATA, PIE_CHART_DATA, \
     RADAR_CHART_DATA, PARALLEL_COORDINATES_DATA_2, BAR_HEATMAP_DATA_2, BAR_RANGE_CHART_DATA_2, SANKEYCHORD_DATA_2, \
@@ -313,7 +312,7 @@ def show_column_chart(request):
     use_default_colors = response_data["use_default_colors"]
     chart_3d = response_data["chart_3d"]
     # TODO: Create a method for getting the actual data from DBs, CSV files, dataframes??
-    data = build_bar_chart(x_axis_name, request)
+    data = build_bar_chart(x_axis_name, request=request)
 
     color_list = define_color_code_list(color_list_request)
     column_chart = XY_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_var_names, y_var_titles, y_var_units,
